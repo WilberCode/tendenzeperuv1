c66645665189d48805ca54677c751f0ee0e61749
WilberCode <parionahuaraca@gmail.com>
2020-04-22 16:36:21 -0500

    cambios en menu mobile

--- single-servicio.php
+++ single-servicio.php
@@ -27,7 +27,7 @@
                 <?php if(have_posts()):
                                 while(have_posts()):  
                                     the_post(); ?>
-                                      <h1  class=" mt-6 text-4xl text-black font-normal " ><?php the_title(); ?> </h1> 
+                                      <h1  class=" mt-6 mb-6 text-4xl text-black font-normal " ><?php the_title(); ?> </h1> 
 						<?php  the_content(); ?>
                 <?php endwhile;
                         else:
