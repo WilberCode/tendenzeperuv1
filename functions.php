b44921bd100d7b7ce3f6dc966ddcc41a525958dc
WilberCode <parionahuaraca@gmail.com>
2020-05-14 01:53:04 -0500

    cambios con customizer

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ use WpTailwindCssThemeBoilerplate\View;
  */
 require get_stylesheet_directory() . '/app/AutoLoader.php'; 
 require get_stylesheet_directory() . '/inc/widgets.php';  
-require get_stylesheet_directory() . '/inc/customizer_fundadoras.php'; 
+// require get_stylesheet_directory() . '/inc/customizer_fundadoras.php'; 
 require get_stylesheet_directory() . '/inc/customizer_asesoria.php'; 
  
 $loader = new AutoLoader();
